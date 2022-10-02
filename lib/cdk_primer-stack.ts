1051c41812b987a7c52a2456ef0d899f92edef1e
Raymond <58218638+raybags-web-dev@users.noreply.github.com>
2022-10-02 23:43:10 +0200

    update README.md

--- lib/cdk_primer-stack.ts
+++ lib/cdk_primer-stack.ts
@@ -1,15 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class CdkPrimerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, "MyCluster", { vpc: vpc });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPrimerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster,
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      publicLoadBalancer: true
+    });
   }
 }
